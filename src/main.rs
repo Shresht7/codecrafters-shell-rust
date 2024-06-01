020ffea0f1ed8d83f2ab1c275de94119b7279354
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-06-01 06:25:40 +0530

    Handle missing commands
    Show an error message to the user that the command they entered doesn't exist.

--- src/main.rs
+++ src/main.rs
@@ -17,4 +17,16 @@ fn main() {
     // Wait for user input and read it into a String variable.
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
+
+    // Split the input into a vector
+    let args: Vec<&str> = input.trim().split_whitespace().collect();
+
+    // Extract the command name from the vector
+    let command = args.get(0);
+
+    // Act on the command-name
+    match command {
+        Some(x) => println!("{}: command not found", x),
+        None => println!("No command provided"),
+    }
 }
