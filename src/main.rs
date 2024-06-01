a03e379130162bd5987ffcb52498a968b18ba04b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-06-01 16:04:16 +0530

    Consolidate stdin reading logic into a separate function
    This change encapsulates the logic of reading user input from stdin into the `read_input` function.

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,8 @@ fn main() {
     // Render the prompt to the screen
     render_prompt("$ ");
 
-    // Wait for user input and read it into a String variable.
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    // Wait for user input and read it into a variable
+    let input = read_input();
 
     // Split the input into a vector
     let args: Vec<&str> = input.trim().split_whitespace().collect();
@@ -35,4 +34,11 @@ fn render_prompt(str: &str) {
     // Flush the output to the screen so the prompt is displayed.
     // The `print!` macro (unlike `println!`) does not flush the output automatically.
     io::stdout().flush().unwrap();
+}
+
+/// Reads the user input from the command line
+fn read_input() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    return input;
 }
