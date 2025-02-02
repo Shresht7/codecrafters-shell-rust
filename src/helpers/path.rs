e15e2b739e717729b0df710d6c1b000a93bced46
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-03 01:49:27 +0530

    Change how errors are handled in `find_executable`

--- src/helpers/path.rs
+++ src/helpers/path.rs
@@ -8,7 +8,7 @@ use std::{env, path::PathBuf};
 /// Find an executable in the `PATH` environment variable.
 pub fn find_executable(name: &str) -> Option<String> {
     // Get the `PATH` environment variable
-    let path = env::var("PATH").unwrap_or_default();
+    let path = env::var("PATH").expect("Failed to retrieve the PATH environment variable");
 
     // Split the `PATH` environment variable into a list of paths
     let paths: Vec<PathBuf> = env::split_paths(&path).collect();
@@ -18,12 +18,12 @@ pub fn find_executable(name: &str) -> Option<String> {
         // Check for name executable in each path
         let executable = path.join(name);
         if executable.exists() {
-            return Some(executable.to_str().unwrap().to_owned());
+            return Some(executable.to_str()?.to_owned());
         }
         // Check name.exe (for windows) while we are at it
         let executable = path.join(format!("{}.exe", name));
         if executable.exists() {
-            return Some(executable.to_str().unwrap().to_owned());
+            return Some(executable.to_str()?.to_owned());
         }
     }
 
